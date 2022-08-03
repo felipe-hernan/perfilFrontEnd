f048c6039750fbcf230fc6b05c460101dd98b9c0
Felipe Burgos <hhh.hb38@gmail.com>
2022-08-03 20:52:29 -0300

    Editar educacion

--- src/app/components/acercade/acercade.component.ts
+++ src/app/components/acercade/acercade.component.ts
@@ -28,4 +28,31 @@ export class AcercadeComponent implements OnInit {
     })
   }
 
+  public abrirModal(modo: String, usuario?: Usuario): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.style.display = 'none';
+    button.setAttribute('data-bs-toggle', 'modal');
+    if (modo === "edit") {
+      this.editarUsuario = usuario;
+      button.setAttribute('data-bs-target', '#editarUsuarioModal');
+    }
+    container?.appendChild(button);
+    button.click();
+
+  }
+  public actualizarUsuario(usuario:Usuario):void {
+    this.editarUsuario = usuario;
+    this.perfilServicio.actualizarUsuario(usuario).subscribe({
+      next: (response: ArrayBuffer) => {
+        console.log(response);
+        this.traerUsuario();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    })
+
+  }
+
 }
--- src/app/servicios/perfil.service.ts
+++ src/app/servicios/perfil.service.ts
@@ -15,8 +15,8 @@ export class PerfilService {
    return this.http.get<Usuario>(`${this.apiServerUrl}/usuario/id/1`);
   }
 
-  public actualizarUsuario(usuario:Usuario):Observable<Usuario>{
-    return this.http.put<Usuario>(`${this.apiServerUrl}/usuario/actualizar`,usuario);
+  public actualizarUsuario(usuario:Usuario):Observable<ArrayBuffer>{
+    return this.http.put<ArrayBuffer>(`${this.apiServerUrl}/usuario/actualizar/1?nombre=${usuario.nombre}&apellido=${usuario.apellido}&titulo=${usuario.titulo}&profecion=${usuario.profecion}&origen=${usuario.origen}&descripcion=${usuario.descripcion}&imagen=${usuario.imagen}`,usuario);
   }
 
 }
